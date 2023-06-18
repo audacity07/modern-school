9ae71a0e32798d5777f83f4a7a045872a10a673c
ajeet p <ajeetpawar07@gmail.com>
2023-06-19 00:32:14 +0530

    worked on media qurery and polished some parts of website

--- scripts/index.js
+++ scripts/index.js
@@ -5,17 +5,32 @@ let firstImg = document.querySelectorAll(".card > img")[0];
 let leftArrow = document.querySelector("#leftArrow");
 let rightArrow = document.querySelector("#rightArrow");
 
-let firstImgWidth = firstImg.clientWidth + 3;
-leftArrow.addEventListener("click", function () {
-  carousel.scrollTo({
-    left: carousel.scrollLeft - firstImgWidth,
-    behavior: "smooth",
+if (window.innerWidth >= 1328) {
+  let firstImgWidth = firstImg.clientWidth + 30;
+  leftArrow.addEventListener("click", function () {
+    carousel.scrollTo({
+      left: carousel.scrollLeft - firstImgWidth,
+      behavior: "smooth",
+    });
   });
-});
-rightArrow.addEventListener("click", function () {
-  carousel.scrollTo({
-    left: carousel.scrollLeft + firstImgWidth,
-    behavior: "smooth",
+  rightArrow.addEventListener("click", function () {
+    carousel.scrollTo({
+      left: carousel.scrollLeft + firstImgWidth,
+      behavior: "smooth",
+    });
   });
-});
-// end of school features carousel
+} else if (window.innerWidth <= 968) {
+  let firstImgWidth = firstImg.clientWidth + 24;
+  leftArrow.addEventListener("click", function () {
+    carousel.scrollTo({
+      left: carousel.scrollLeft - firstImgWidth,
+      behavior: "smooth",
+    });
+  });
+  rightArrow.addEventListener("click", function () {
+    carousel.scrollTo({
+      left: carousel.scrollLeft + firstImgWidth,
+      behavior: "smooth",
+    });
+  });
+}
